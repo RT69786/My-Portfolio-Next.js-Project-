546e2b92c6e3fe54268b555c9825c931b37f2a01
Ashar Hamid <asharhamid112233@gmail.com>
2026-07-31 16:09:34 +0500

    just remove main03 from page.jsx file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header/Header";
 import Main01 from "./components/Main01/Main01";
 import Main02 from "./components/Main02/Main02";
 import Main04 from "./components/Main04/Main04";
-import Main03 from "./components/Main03/Main03";
+
 
 import Footer from "./components/Footer/Footer";
 
@@ -21,7 +21,7 @@ export default function Home() {
         <Main02 />
       </section>
 
-      <Main03 />
+     
 
       <Main04 />
 
